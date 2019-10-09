121f130565aa3a6bcad70f0dbf67a59992ef4dba
nguyendacthienngan <nguyendacthienngan@gmail.com>
2019-10-09 18:38:43 +0700

    Update Solution (UI+UX)

---
+++ RestaurantManagement/GUI/GUI/UCTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using Model;
+namespace GUI
+{
+    public partial class ucTable1 : UserControl
+    {
+        TableBLL tableBLL;
+        public ucTable1()
+        {
+            tableBLL = new TableBLL();
+            InitializeComponent();
+            LoadData();
+        }
+        void LoadData()
+        {
+            //dataGridView1.AutoGenerateColumns = false;
+            //List<Table> lstTable = tableBLL.GetListTable();
+            //dataGridView1.DataSource = lstTable;
+
+            //DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            //column.DataPropertyName = "TableName";
+            //column.Name = "Tên bàn";
+            //dataGridView1.Columns.Add(column);
+
+
+            //column = new DataGridViewTextBoxColumn();
+            //column.DataPropertyName = "Status";
+            //column.Name = "Trạng thái";
+            //dataGridView1.Columns.Add(column);
+        }
+    }
+}
