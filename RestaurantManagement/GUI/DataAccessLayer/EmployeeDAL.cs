89281f1971a421118d6d86b887f484d526ed9f77
Tiên Ung Bảo <ungbaotien0512@gmail.com>
2019-11-07 10:40:19 +0700

    Update EmployeeDAL.cs

---
+++ RestaurantManagement/GUI/DataAccessLayer/EmployeeDAL.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class EmployeeDAL
+    {
+        RestaurantContextEntities dbContext;
+        public EmployeeDAL()
+        {
+            dbContext = new RestaurantContextEntities();
+        }
+        public List<Employee> GetListEmployee()
+        {
+            return dbContext.Employees.ToList();
+        }
+    }
+}
